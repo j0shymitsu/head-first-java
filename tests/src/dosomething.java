98ba47667d411bbc500967c2d1c52485dbdf6b46
Josh Birch <2312292@chester.ac.uk>
2024-08-16 11:19:58 +0100

    Conditionals

--- tests/src/dosomething.java
+++ tests/src/dosomething.java
@@ -3,7 +3,7 @@ public class dosomething {
         int x = 3;
         String name = "Josh";
         x = x * 17;
-        System.out.print("x is " + x + " - ");
+        System.out.println("x is " + x);
         double d = Math.random();
         // This is a comment.
 
@@ -13,7 +13,18 @@ public class dosomething {
         }
 
         for (x = 0; x < 10; x = x + 1){
-            System.out.print("x is now " + x + ", ");
+            System.out.println("x is now " + x);
         }
+
+        // Conditionals
+        if (x == 10){
+            System.out.println("x must be 10.");
+        } else{
+            System.out.println("x isn't 10.");
+        }
+        if ((x < 3) & (name.equals("Josh"))){
+            System.out.println("Hello!");
+        }
+        System.out.println("This line returns no matter what.");
     }
 }
