4adc3e0ae91b9f8ae22aaf66b27174ceb50ff5d2
Josh Birch <2312292@chester.ac.uk>
2024-08-16 11:15:07 +0100

    Basic loops

--- tests/src/dosomething.java
+++ tests/src/dosomething.java
@@ -3,8 +3,17 @@ public class dosomething {
         int x = 3;
         String name = "Josh";
         x = x * 17;
-        System.out.print("x is "+ x);
+        System.out.print("x is " + x + " - ");
         double d = Math.random();
         // This is a comment.
+
+        // Basic loops
+        while (x > 12){
+            x = x - 1;
+        }
+
+        for (x = 0; x < 10; x = x + 1){
+            System.out.print("x is now " + x + ", ");
+        }
     }
 }
