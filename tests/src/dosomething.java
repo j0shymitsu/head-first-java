34351808acfaa1100d3e05be2d7d8ebe8a450a37
Josh Birch <2312292@chester.ac.uk>
2024-08-16 11:11:27 +0100

    Initial program

---
+++ tests/src/dosomething.java
@@ -0,0 +1,10 @@
+public class dosomething {
+    public static void main(String[] args) throws Exception {
+        int x = 3;
+        String name = "Josh";
+        x = x * 17;
+        System.out.print("x is "+ x);
+        double d = Math.random();
+        // This is a comment.
+    }
+}
