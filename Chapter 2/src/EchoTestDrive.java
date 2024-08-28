884680fe6f50d6ed60743e16394c7a6859333414
Josh Birch <2312292@chester.ac.uk>
2024-08-28 10:37:50 +0100

    Pool puzzle (Echo) [UNFINISHED]

---
+++ Chapter 2/src/EchoTestDrive.java
@@ -0,0 +1,27 @@
+public class EchoTestDrive 
+{
+    public static void main(String[] args) 
+    {
+        Echo e1 = new Echo();
+        //
+        int x = 0;
+        while("//")
+        {
+            e1.hello();
+            //
+            
+            if ("//")
+            {
+                e2.count = e2.count + 1;
+            }
+            
+            if ("//")
+            {
+                e2.count = e2.count + e1.count;
+            }
+
+            x = x + 1;
+        }    
+        System.out.println(e2.count);
+    }
+}
