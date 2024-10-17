9eeeda76a59c8c44eddaa47827836ed4193df47d
Josh Birch <2312292@chester.ac.uk>
2024-10-17 09:06:57 +0100

    Edit chapter 4 exercise on arrays
    Made it more readable/easier to understand.

--- 4. How Objects behave/src/Puzzle4.java
+++ 4. How Objects behave/src/Puzzle4.java
@@ -1,46 +1,46 @@
+class MultiplierObject
+{
+    int value;
+
+    public int calculateValue(int factor)
+    {
+        if(value > 100)    // If value is greater than 100, multiply by factor; else multiply by (5-factor)
+        {
+            return value * factor;
+        }
+        else
+        {
+            return value * (5 - factor);
+        }
+    }
+}
+
 public class Puzzle4 
 {
     public static void main(String[] args) 
     {
 
-        Puzzle4b[] obs = new Puzzle4b[6];
-        int y = 1;
-        int x = 0;
-        int result = 0;
+        MultiplierObject[] multiplierObjects = new MultiplierObject[6];    // Creates an array to hold 6 multiplier objects
+        int multiplier = 1;
+        int arrayIndex = 0;
+        int totalSum = 0;
         
-        while(x < 6)
+        
+        while(arrayIndex < 6)    // Initialise array with MultiplierObject instances
         {
-            obs[x] = new Puzzle4b();
-            obs[x].ivar = y;
-            y = y * 10;
-            x = x + 1;
+            multiplierObjects[arrayIndex] = new MultiplierObject();
+            multiplierObjects[arrayIndex].value = multiplier;
+            multiplier = multiplier * 10;
+            arrayIndex = arrayIndex + 1;
         }    
+        arrayIndex = 6;
 
-        x = 6;
-
-        while(x > 0)
+        while(arrayIndex > 0)    // Calculate the sum by calling calculateValue() on each object
         {
-            x = x - 1;
-            result = result + obs[x].doStuff(x);
+            arrayIndex = arrayIndex - 1;
+            totalSum = totalSum + multiplierObjects[arrayIndex].calculateValue(arrayIndex);
         }
-
-        System.out.println("result " + result);
+        System.out.println("Total sum: " + totalSum);
     }    
 }
 
-class Puzzle4b
-{
-    int ivar;
-
-    public int doStuff(int factor)
-    {
-        if(ivar > 100)
-        {
-            return ivar * factor;
-        }
-        else
-        {
-            return ivar * (5 - factor);
-        }
-    }
-}
