d7a4874e814a2219dd89e3628833fa596d31b389
Josh Birch <joshuabirch@protonmail.com>
2025-05-22 08:29:10 +0100

    Fix flaw in BeerSong.java, refactor old code

--- src/com/josh/headfirst/chapter1/BeerSong.java
+++ src/com/josh/headfirst/chapter1/BeerSong.java
@@ -1,25 +1,34 @@
 package com.josh.headfirst.chapter1;
 
-public class BeerSong {
-    public static void main (String[] args){
+public class BeerSong
+{
+    public static void main (String[] args)
+    {
         int beerNum = 99;
         String word = "bottles";
 
-        while (beerNum > 0){
-
-            if (beerNum == 1){
-                word = "bottle";
-            }
-
+        while (beerNum > 0)
+        {
             System.out.println(beerNum + " " + word + " of beer on the wall,");
             System.out.println(beerNum + " " + word + " of beer.");
             System.out.println("Take one down");
             System.out.println("Pass it around.");
             beerNum = beerNum - 1;
+            if (beerNum == 1)
+            {
+                word = "bottle";
+            }
+            else
+            {
+                word = "bottles";
+            }
 
-            if (beerNum > 0){
+            if (beerNum > 0)
+            {
                 System.out.println("Now there's " + beerNum + " " + word + " of beer on the wall.\n");
-            } else{
+            }
+            else
+            {
                 System.out.println("No more bottles of beer on the wall.");
             }
         }
